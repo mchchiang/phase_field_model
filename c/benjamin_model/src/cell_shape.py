bd054eff7230e556bc7b17eed6fc601accaaf810
Michael Chiang <michael_chiang_ch@hotmail.com>
2019-04-16 22:26:56 +0100

    Add code to generate triangle lattice points and initial cell phase fields

---
+++ c/benjamin_model/src/cell_shape.py
@@ -0,0 +1,53 @@
+# cell_shape.py
+# Genreate a simple shape of a cell on its own lattice
+
+import argparse
+import numpy as np
+
+parser = argparse.ArgumentParser()
+parser.add_argument("nx", type=int, help="width of lattice")
+parser.add_argument("ny", type=int, help="height of lattice")
+parser.add_argument("phi0", type=float, help="maximum value of phi")
+parser.add_argument("outfile", help="name of output file")
+subparsers = parser.add_subparsers(title="shapes",dest="shape")
+parser_circle = subparsers.add_parser("circle")
+parser_square = subparsers.add_parser("square")
+parser_circle.add_argument("radius", type=float, help="radius of the cell")
+parser_square.add_argument("length", type=float, help="length of the cell")
+args = parser.parse_args()
+
+nx = args.nx
+ny = args.ny
+phi0 = args.phi0
+outfile = args.outfile
+
+def circle(x, y, r):
+    x0 = x/2
+    y0 = y/2
+    phi = [[phi0 if ((i-x0)**2+(j-y0)**2)**0.5 < r else 0.0 \
+            for j in xrange(ny)] for i in xrange(nx)]
+    return phi
+
+def square(x, y, l):
+    x0 = x/2
+    y0 = y/2
+    lh = l/2
+    phi = [[phi0 if (abs(i-x0)<lh and abs(j-y0)<lh) else 0.0 \
+            for j in xrange(ny)] for i in xrange(nx)]
+    return phi
+
+def zeros(x, y):
+    phi = [[0.0 for j in xrange(ny)] for i in xrange(nx)]
+    return phi
+
+if args.shape == "circle":
+    phi = circle(nx, ny, args.radius)
+elif args.shape == "square":
+    phi = square(nx, ny, args.length)
+else:
+    phi = zeros(nx, ny)
+
+with open(outfile, "w") as writer:
+    for i in xrange(nx):
+        for j in xrange(ny):
+            writer.write("{:d} {:d} {:.5f}\n".format(i, j, phi[i][j]))
---
+++ c/benjamin_model/src/triangle.py
@@ -0,0 +1,30 @@
+# triangle.py
+# Genreate the coordinates of the cells' centre of mass which are
+# arranged in a triangular lattice
+
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("nx", type=int, help="number of cells in each row")
+parser.add_argument("ny", type=int, help="number of cells in each col")
+parser.add_argument("radius", type=float, help="radius of each cell")
+parser.add_argument("outfile", help="name of output file")
+args = parser.parse_args()
+
+nx = args.nx
+ny = args.ny
+r = args.radius
+outfile = args.outfile
+
+ncells = nx*ny
+
+lx = int(2*r*nx)
+ly = int(3**0.5*r*ny)
+
+print "lx = {:d} ly = {:d}".format(lx, ly)
+
+with open (outfile, "w") as writer:
+    for i in xrange(0,ncells):
+        x = r*((i/nx)%2) + 2*r*(i%nx)
+        y = 3**0.5*r*(i/nx)
+        writer.write("{:d} {:.5f} {:.5f}\n".format(i,x,y))
