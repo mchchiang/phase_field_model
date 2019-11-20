b545d78accbbd21dbf8586c01eb93100872935fe
Michael Chiang <michael_chiang_ch@hotmail.com>
2019-11-20 17:06:23 +0000

    Add code to compute nearest neighbours based on delaunay triangulation

---
+++ c/benjamin_model/analysis/run/neigh_delaunay.sh
@@ -0,0 +1,81 @@
+#!/bin/bash
+d_start=$1
+d_end=$2
+d_inc=$3
+pe_start=$4
+pe_end=$5
+pe_inc=$6
+run_start=$7
+run_end=$8
+run_inc=$9
+in_dir=${10}
+out_dir=${11}
+
+if [ "$#" != 11 ]; then
+    echo "usage: neigh_delaunay.sh d_start d_end d_inc pe_start pe_end pe_inc run_start run_end run_inc in_dir out_dir"
+    exit 1
+fi
+
+if [ ! -d $outdir ]; then
+    mkdir -p $outdir
+fi
+
+d=$(python -c "print '%.3f' % ($d_start)")
+pe=$(python -c "print '%.3f' % ($pe_start)")
+
+neigh_py="../src/neigh_delaunay.py"
+
+N=100 #100
+tstart=0
+tend=21000000
+tinc=1000
+
+max_jobs=8 # 8
+cmd=()
+jobid=0
+
+while (( $(bc <<< "$d < $d_end") ))
+do
+    in_path="${in_dir}/d_${d}/"
+    if [ -d $in_path ]; then
+	out_path="${out_dir}/d_${d}/neigh_delaunay/"
+	if [ ! -d $out_path ]; then
+	    mkdir -p $out_path
+	fi
+	pe=$(python -c "print '%.3f' % ($pe_start)")
+	while (( $(bc <<< "$pe < $pe_end") ))
+	do
+	    for (( run=$run_start; $run<=$run_end; run+=$run_inc ))
+	    do
+		name="cell_N_${N}_d_${d}_Pe_${pe}_run_${run}"
+		pos_file="${in_path}/position/pos_${name}.dat"
+		if [ -f $pos_file ]; then
+		    params_file="${in_path}/siminfo/params_${name}.txt"
+		    lx=$(grep 'lx = ' $params_file | awk '{print $3}')
+		    ly=$(grep 'ly = ' $params_file | awk '{print $3}')
+		    neigh_file="${out_path}/neigh_${name}.dat"
+		    cmd[$jobid]="python3 $neigh_py $N $lx $ly $tstart $tend $tinc $pos_file $neigh_file"
+		    jobid=$(bc <<< "$jobid + 1")
+		fi
+	    done
+	    pe=$(python -c "print '%.3f' % ($pe + $pe_inc)")
+	done
+    fi
+    d=$(python -c "print '%.3f' % ($d + $d_inc)")
+done
+
+# Parallel runs
+
+total_jobs=$jobid
+jobid=0
+
+while (( $(bc <<< "$jobid < $total_jobs") ))
+do
+    for (( i=0; i<$max_jobs && $jobid < $total_jobs; i++))
+    do
+	echo "${cmd[jobid]} &"
+	${cmd[jobid]} &
+	jobid=$(bc <<< "$jobid + 1")
+    done
+    wait
+done
---
+++ c/benjamin_model/analysis/src/neigh_delaunay.py
@@ -0,0 +1,94 @@
+# neigh_delaunay.py
+# A script to find the nearest neighbours of a cell using Delaunay 
+# triangulation. 
+
+import sys
+
+# This code must be run with python3!
+if (sys.version_info < (3, 5)):
+    print("This code mus tbe run with Python version 3.5 or higher")
+    sys.exit(1)
+
+import numpy as np
+from scipy.spatial import Delaunay
+
+args = sys.argv
+if (len(args) != 9):
+    print("usage: neigh_delaunay.py npoints lx ly tstart tend tinc pos_file out_file")
+    sys.exit(1)
+
+npoints = int(args.pop(1))
+lx = int(args.pop(1))
+ly = int(args.pop(1))
+tstart = int(args.pop(1))
+tend = int(args.pop(1))
+tinc = int(args.pop(1))
+pos_file = args.pop(1)
+out_file = args.pop(1)
+xbuff = 0.2*lx
+ybuff = 0.2*ly
+nframes = int((tend-tstart)//tinc+1)
+
+periodic_loc = [(lx,-ly),(lx,0),(lx,ly),(0,-ly),(0,ly),(-lx,-ly),
+                (-lx,0),(-lx,ly)]
+
+# Read position data
+nlines = npoints+2
+reader = open(pos_file, 'r')
+writer = open(out_file, 'w')
+
+while True:
+    # Read header sectoin (inlcuding time info)
+    for i in range(2):
+        line = reader.readline()
+    if (not line): break
+    data = line.split()
+    time = int(data[1])
+    if (time > tend):
+        break
+    elif (time < tstart or (time-tstart)%tinc != 0):
+        for i in range(npoints):
+            line = reader.readline()
+    else:
+        pos = []
+        index_map = [n for n in range(npoints)]
+        frame = int((time-tstart)//tinc)
+        for n in range(npoints):
+            line = reader.readline()
+            data = line.split()
+            x = float(data[0])
+            y = float(data[1])
+            if (x < lx+xbuff and x > -xbuff and y < ly+ybuff and y > -ybuff):
+                pos.append((x,y))
+        for n in range(npoints):
+            x,y = pos[n]
+            for pt in periodic_loc:
+                xp = x+pt[0]
+                yp = y+pt[1]
+                if (xp < lx+xbuff and xp > -xbuff and yp < ly+ybuff and 
+                    yp > -ybuff):
+                    pos.append((xp,yp))
+                    index_map.append(n)
+        pos = np.array(pos)
+        
+        # Perform delaunay triangulation
+        tri = Delaunay(pos)
+
+        # Create neighbour lists
+        neighbours = [set() for i in range(len(pos))]
+        for s in tri.simplices:
+            for i in s:
+                for j in s:
+                    if (i != j):
+                        neighbours[i].add(index_map[j])
+        
+        # Output result
+        writer.write("Cells: {:d}\n".format(npoints))
+        writer.write("Timestep: {:d}\n".format(time))
+        for i in range(npoints):
+            for n in sorted(neighbours[i]):
+                writer.write("{:d} ".format(n))
+            writer.write("\n")
+
+reader.close()
+writer.close()
